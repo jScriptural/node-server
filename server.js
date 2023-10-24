0be80afad3d07565219e1da33d753eb6b18a1476
jscriptural <isongjohn014@gmail.com>
2023-10-24 11:48:49 +0100

    set up start script and heroku port

--- server.js
+++ server.js
@@ -26,10 +26,11 @@ app.use((req,res,next)=>{
     next()
 
 })
+/*
 app.use((req,res,next)=>{
     res.render("maintenance.hbs");
 })
-
+*/
 app.use(express.static(__dirname+"/public"));
 //console.log(app);
 
